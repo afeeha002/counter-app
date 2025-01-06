2cdff1ccbd79e2b8a144ddad166d1368ee1ac8fb
afeeha <afeehahamza@gmail.com>
2025-01-06 15:54:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Number from './components/Number'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="counter-container">
+        <h2>React Counter App</h2>
+        <Number />
+
+
+
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Number.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+
+const Number = () => {
+
+    const [count, setCount] = useState(0);
+    const [step, setStep] = useState(1);
+
+    const increment = () => {
+        setCount(count + step);
+    };
+
+    const decrement = () => {
+        setCount(count - step);
+    };
+
+    const zero = () => {
+        setCount(0);
+        setStep(1);
+    };
+    const stepChange = (steps) => {
+        const value = parseInt(steps.target.value, 10);
+        setStep(isNaN(value) ? 1 : value);
+
+
+    }
+
+    return (
+        <>
+            <h1>{count}</h1>
+
+            <button onClick={increment} type="button" class="m-2 btn btn-warning">increment</button>
+
+            <button onClick={decrement} type="button" class="m-2 btn btn-warning">Decrement</button>
+
+            <button onClick={zero} type="button" class="m-2 btn btn-warning">Reset</button>
+
+            <div class="step">
+                <label for="step">Step:</label>
+                <input type="number" id="step" value={step}
+                    onChange={stepChange} />
+            </div>
+        </>
+    )
+}
+
+export default Number
